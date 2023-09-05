0802a60b50071a39bc2318c67e3de802eaba7aee
jayakviya <143339476+jayakviya@users.noreply.github.com>
2023-09-05 17:01:34 +0530

    Update Giving balance to each.py

--- Balance.py
+++ Balance.py
@@ -2,6 +2,8 @@ a = 1000
 b = 1200
 c = 800
 amount = a+b+c
+#amount is 3000 and we purchased 2000.
+#This program is determined to calculate the returning amount for each person
 purchased_amount = 2000
 balance = amount - purchased_amount
 a1 = balance/3
