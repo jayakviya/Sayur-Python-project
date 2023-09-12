afbdfbba933f392d96d99432808daf19de5b2eec
[jayakviya] <[uma04367@gmail.com]>
2023-09-12 19:20:43 +0530

    to transfer

--- #swap two numbers.py
+++
@@ -1,9 +0,0 @@
-#swap two numbers
-a = int(input("Enter the first number :"))
-b = int(input("Enter the Second number :"))
-a,b = b,a
-print("Before swapping")
-print("a is",a,"b is",b)
-print("After swapping")
-print("a is",b,"b is",a)
-
