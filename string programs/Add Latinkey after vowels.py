0a516ecb91349e69b86998c8965c055335f4368e
[jayakviya] <[uma04367@gmail.com]>
2023-09-16 10:00:56 +0530

    Adding new

---
+++ string programs/Add Latinkey after vowels.py
@@ -0,0 +1,6 @@
+########## Program 4 ##########
+#PigLatin - From the input string, for each word, remove the first chars until a vowel, add it to the end of the word
+#and add 'ay' to it.
+#eg I am Python
+#answer Iay maay nPythoay (in Python 'o' is the first vowel)
+
