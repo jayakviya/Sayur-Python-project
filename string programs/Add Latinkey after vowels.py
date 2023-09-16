e163d0315478f78f10dc8a13d9cdc56add0c2d80
[jayakviya] <[uma04367@gmail.com]>
2023-09-16 11:04:45 +0530

    adding some codes

--- string programs/Add Latinkey after vowels.py
+++ string programs/Add Latinkey after vowels.py
@@ -4,3 +4,30 @@
 #eg I am Python
 #answer Iay maay nPythoay (in Python 'o' is the first vowel)
 
+inputSentence = input("Enter your input :") #get the input
+
+piglatinkey = "ay" #initialise the key
+
+vowels =["a","e","i","o","u"] #arrange vowels in array
+sentence ="" 
+outputSentence =[] # to store the output sentence
+for word in inputSentence.split(" "): # split the inputsentence and using for llop for every word in the sentece 
+    vowelIndex = -1 # intialise to =1 it indicates that no vowels in the word
+    
+    for index,letter in enumerate(word): #using for and if stm to find out the place of the vowel letter
+        if letter.lower() in vowels:
+            vowelIndex =  index #to store the index value of the vowel
+            break
+    if vowelIndex != -1: # if there is a vowel sentence 
+             
+        sentence = sentence + word[vowelIndex+1:] + word[:vowelIndex+1 ] +piglatinkey + " "
+    else:
+        sentence = word
+outputSentence.append(sentence)  # to add the sentence to output sentence
+print("The output sentence is ".join(outputSentence)) # use the key join to print the outputsentence  
+
+'''
+output
+------
+Enter your input :we are just friends
+weay reaay stjuay endsfriay '''
