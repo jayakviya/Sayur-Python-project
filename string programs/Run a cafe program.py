375337bc126bc37c926bf209debfe9fcac85dd0b
[jayakviya] <[uma04367@gmail.com]>
2023-09-17 10:38:27 +0530

    change

---
+++ string programs/Run a cafe program.py
@@ -0,0 +1,40 @@
+########### Program 5 #######
+  # Write a program to run a cafe
+  #  Cafe should have inventory, sales data and profit
+
+
+total_costPrize=500
+Cafe_Menu=["coffee","tea","greentea","blacktea"]
+#ordered_list=[]
+Cafe_price=[20,15,30,45]
+total=0
+b=True
+print("Welcome to our shop!")
+print("Please let us know what do you want")
+print("Here our menu card :")
+for i,food in enumerate(Cafe_Menu):
+    print(f" {i+1} - {food}")
+print()
+while True:
+    item=input("Enter your food item")
+    order=""
+    b=True
+    for i in range(len(Cafe_Menu)):
+        if (item==Cafe_Menu[i]):
+            quantity = int(input(f"Enter number of {item} you need ? "))
+            order=str(quantity)+" "+item+" and it price is "+str(quantity*Cafe_price[i])          
+            total=total+(Cafe_price[i]*quantity)
+            ordered_list.append(order)
+            b=False
+            break
+    if b:
+        print(f"Sorry sir...{item} is not available")
+    if int(input("Enter 1 to cancel order or 0 to continue ordeer :")):        
+        break
+    print()
+print(f"Your Ordered list : {ordered_list}")
+print(f"Your total bill amount is : {total}")
+if total<total_costPrize:
+    print(f"You had loss because your total profit = {total} is < total invested amount per day = {total_costPrize}")
+elif total>total_costPrize:
+    print(f"You had get profit your total profit = {total} is > total invested amount per day = {total_costPrize}")
