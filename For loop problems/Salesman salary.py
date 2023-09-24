29099dfd4457a2a0e4138cd7b7d1c477b015b9b3
[jayakviya] <[uma04367@gmail.com]>
2023-09-24 10:51:40 +0530

    add more

--- For loop problems/Salesman salary.py
+++ For loop problems/Salesman salary.py
@@ -9,28 +9,28 @@
 
 monthlySalesList = [5,23,21,14,23,12,4,12,22,22,34,12]  # Sample number of phones sold in each month in a year
 #FillinMissingCode - initialise all the variables needed
-currentMonthSalary= 10000
+baseSalary= 10000
 previousMonthSalary = 0
 cumulativeBonus = 0
 for month, phoneCount in enumerate(monthlySalesList):
-    print (f"{month+1} month's salary before additional bonus {currentMonthSalary}")
+    print (f"{month+1} month's salary before additional bonus {baseSalary}")
     if phoneCount>=5:
-        currentMonthSalary+=5000+(phoneCount-5)*1100
+        currentMonthSalary= baseSalary+5000+(phoneCount-5)*1100
     else:
-        currentMonthSalary=currentMonthSalary
+        currentMonthSalary=baseSalary
  
     if (previousMonthSalary >= 20000) and (phoneCount >=20) :
         currentMonthSalary+=5000
     else:
         currentMonthSalary=currentMonthSalary
-    cumulativeBonus = currentMonthSalary-10000
-    print("This month bonus =",cumulativeBonus)
+    cumulativeBonus += currentMonthSalary-10000
+    print("Bonus Amount =",cumulativeBonus)
     
     if cumulativeBonus >= 100000:
         currentMonthSalary+=currentMonthSalary*0.05
     print (f"{month+1} month's salary after additional bonus {currentMonthSalary}")
     previousMonthSalary = currentMonthSalary
-        
+    print("_____________________")       
 
 
 
