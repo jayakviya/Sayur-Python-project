e449e75a5390a6726f89ec253cc478ff47edcda4
[jayakviya] <[uma04367@gmail.com]>
2023-09-24 12:44:55 +0530

    add

--- For loop problems/ask money for movie.py
+++ For loop problems/ask money for movie.py
@@ -11,19 +11,28 @@
 
 money = 0
 askMoney = int(input("I want to go fora movie please give me money:"))
-for i in range(5):
+for i in range(4):
     if askMoney < 1000:
         if askMoney > 10:
             money += askMoney
-            print("Thank you!")
-            print("Gimme some more money :")
+            if money <  1000:
+            
+                print("Thank you!")
+                askMoney = int(input("Gimme some more money :"))
+            else :
+                print("Thank you I have enough money")
+                print(f"{i+2} times I ask for money")
+            if (i+2)==5 :
+                print("you asked money for 5 times and it is the limited time")
+            else:
+                print('You may ask one more time')
+
+            
         else:
             money=money
-        if askMoney >= 1000:
-            print("Thank you I have enough money")
-            print(f"{i+1} times I ask for money")
-            break
-        else:
+            askMoney = int(input("Gimme some more money :"))
             continue
-
+    
+    
+        
 
