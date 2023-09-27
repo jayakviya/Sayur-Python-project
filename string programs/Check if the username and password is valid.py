53879fd0e555be8a568d8ba65b49b22da2fcedde
[jayakviya] <[uma04367@gmail.com]>
2023-09-27 18:58:38 +0530

    add

---
+++ string programs/Check if the username and password is valid.py
@@ -0,0 +1,13 @@
+''' Check if the username and password is correct. 
+     Username should contain the char @  and '.com' or '.edu' or '.tech' or 'org' at the end.
+     passward is the first, third, and last 3 letters of the username followed by the first three letters of the 
+     name of the company mentioned in the username, followed by any 3 numbers.
+     eg username : myname@sayur.com. password - mnamesay123 
+     '''
+
+import re
+
+userName = input("Enter user name:")
+#passWord = input("Enter your password")
+check = re.search(r'@(.com|.edu|.tech|.uma@gmail.coorg)',userName)
+print(check)
---
+++ string programs/String manipulation.py
@@ -0,0 +1,22 @@
+'''
+String manipulation exercises. Use builtin functions as needed.
+
+1. Write an app for the fruit vendor. Fruit vendor has a list of fruits (apple, Orange, banana etc).
+When the customer comes in, vendor asks "What do you want to buy?" .
+The customer can say "I want apple", or "Apple" or "three apple" or something like that.
+Your program will find out what fruit the customer is asking. 
+Your program will also find, if the customer already asked for the quantity of the fruit (one apple or 5 orange etc).
+Print the quantity if the customer says the quantity. If not, ask him how much he wants.
+Hint : Use string manipulation and lists. https://www.w3schools.com/python/python_ref_string.asp 
+You can limit the quantity to single digit number.
+
+2. Input is an encrypted  string where there will be chars followed by number. The way to decrypt is 
+to repeat the chars, by the number of times. Print the decrypted word and its length. If there are any special 
+chars, all the chars between the number and special char are ignored.
+ eg - ac2bd3 means acacbdbdbd. 
+ ac2acc#cd3 acaccdcdcd
+
+3. Write the function mostFrequentLetters(s), that takes a string s, and ignoring case (so "A" and "a" are treated the same), returns a lowercase string containing the letters of s in most frequently used order. (In the event of a tie between two letters, follow alphabetic order.)
+Eg - "We Attack at Dawn" is input. Output should be 'atwcdekn'
+Do not use dictionaries. Try to use string built in functions.
+'''
