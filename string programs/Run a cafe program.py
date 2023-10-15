4a86a01e4db10822a6f47bf8b9772120bff968cf
[jayakviya] <[uma04367@gmail.com]>
2023-10-15 19:59:22 +0530

    chande

--- string programs/Run a cafe program.py
+++ string programs/Run a cafe program.py
@@ -2,55 +2,58 @@
   # Write a program to run a cafe
   #  Cafe should have inventory, sales data and profit
 import re 
-menu = {"coffee":{'quantity':50,'price':20,'profit':10},"tea":{'quantity':30,'price':30,'profit':5},
-        "greentea":{'quantity':30,'price':50,'profit':10},"blacktea":{'quantity':45,'price':70,'profit':30}}
-total_cost = 4250 
+#define menu card as dictionary it contains the available quantity and the price of the corresponding item
 
-total = 0 # initialise the total                  #2300 - total profit value
+menu = {"coffee":{'quantity':50,'price':20},"tea":{'quantity':30,'price':30},
+        "greentea":{'quantity':30,'price':50},"blacktea":{'quantity':45,'price':70}}
+total_cost = 4500 #initialize the cost value
+
+total = 0 # initialise the total                  
 order = 0 
 
 
-while order < 2:
+while order <= 10:#I have given the limited value to the while loop
 
     item=input("what do you want?")
+#bring the input from the user
 
+    items = re.findall(r'(\d+)\s*(\w+)',item)#seperate the quantity and item as tuples.
 
-    items = re.findall(r'(\d+)\s*(\w+)',item)
-
-    for quantity ,item_name in items:
+    for quantity ,item_name in items:#for loop will iterate the no of given items
         
-        quantity = int(quantity)
+        quantity = int(quantity)#type casting the string quantity value into integer
 
-        if item_name in menu:
+        if item_name in menu:#check if the given item is in our cafe
 
-            item_details = menu[item_name]
+            item_details = menu[item_name]# to collect the details of item
             
-            item_price = menu[item_name]['price']
+            item_price = menu[item_name]['price']#to store the price of item
            
-
+#check if the quantity of item that user want is available in our cafe
             if item_details['quantity'] >=quantity:
+
                 print(f' {quantity} {item_name} is {item_price*quantity} ')
 
-                total += quantity * item_price
-                item_details['quantity'] -= quantity
-                order -= 1
+                total += quantity * item_price     #calculate and store the value of total 
+                item_details['quantity'] -= quantity  #to reduce the quantity of item in the menu because it sold
+                order += 1  
                
-
+#if the required quantity is not available in our cafe then this part execute
             else:
-                (f'Sorry {item_name} is not in stock in sufficient quantity')
+                print(f'Sorry {item_name} is not in stock in sufficient quantity')
+#if the item is not in the menu then the following will execute
         else:
             print(f'Your order {item_name} is not in the menu card')
-    choice = input('Do you want to continue your order press 1 or press 0 to exit')
-    if choice == 0:
+#ask the user if he/she want to buy else 
+    choice = int(input('Do you want to continue your order press 1 or press 0 to exit'))#to store the user's choice
+    if choice == 0:#choice is 0 then loop breaks otherwise it continues
         break
     else:
         continue
-
-print(f"Your total bill amount is {total}")
-for items,details in menu.items():
-     profit = item[details]['profit']*item[details]['quantity'] - menu[item_name]['quantity']
-     
-     print(f'Profit fot {item_name} is {profit}')
-
-            
+    
+print(f"Your total bill amount is {total}")#to print the total amount 
+if total >= total_cost:#to check if we get lose or profit
+    print("You get profit")
+else:
+    print("you get loss")
     
